1feda334246ba5d606780a45047151d6e2673e4a
Nandha Official <nandha10799@gmail.com>
2024-10-30 09:50:27 +0530

    bug fixing

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -552,6 +552,195 @@
 
 
 
+// import React, { useState, useEffect, useRef } from "react";
+// import { motion } from "framer-motion";
+// import { FaRegEnvelope, FaChevronDown } from "react-icons/fa";
+// import { Link, useLocation } from "react-router-dom";
+// import HamburgerMenu from "./hamburgar";
+
+// const Navbar: React.FC = () => {
+//   const [scrolled, setScrolled] = useState(false);
+//   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+//   const dropdownRef = useRef<HTMLDivElement | null>(null);
+//   const location = useLocation();
+
+//   // Handle scroll effect for background change
+//   const handleScroll = () => {
+//     setScrolled(window.scrollY > 50);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener("scroll", handleScroll);
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   // Close dropdown if clicking outside
+//   useEffect(() => {
+//     const handleClickOutside = (event: MouseEvent) => {
+//       if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+//         setIsDropdownOpen(false);
+//       }
+//     };
+//     document.addEventListener("mousedown", handleClickOutside);
+//     return () => {
+//       document.removeEventListener("mousedown", handleClickOutside);
+//     };
+//   }, []);
+
+//   const isActive = (path: string) => location.pathname === path;
+
+//   // Conditionally render Navbar only on specific pages
+//   if (location.pathname === "/news" || location.pathname === "/testimonials") {
+//     return null;
+//   }
+
+//   return (
+//     <nav
+//       className={`fixed w-full top-0 left-0 z-[1000] transition-all duration-500 ease-out
+//       ${
+//         scrolled
+//           ? "bg-white/5 bg-opacity-70 backdrop-blur-md shadow-lg h-[10%]"
+//           : "bg-transparent h-[15%]"
+//       } 
+//       flex items-center justify-center px-8 lg:px-[10rem]`}
+//     >
+//       <div className="flex items-center w-full h-full">
+//         <div className="w-[50%] md:w-[20%] lg:w-[20%] h-full">
+//           <a href="#home" className="w-full h-full flex justify-center items-center">
+//             <img src="./game_on_logo.webp" alt="Logo" className="w-full" />
+//           </a>
+//         </div>
+
+//         <div className="hidden bg-[#181817] w-[70%] lg:w-[70%] md:bg-transparent md:flex h-full items-center justify-center transition-all duration-500 ease-out">
+//           <ul className="flex flex-col items-center justify-center w-full gap-8 lg:gap-16 text-white md:flex-row md:w-auto font-secondary">
+//             <li>
+//               <a
+//                 href="#home"
+//                 className={`font-medium uppercase ${
+//                   isActive("/#home") || location.pathname === "/" ? "text-secondary" : "text-[#D2D2D0]"
+//                 } transition-all hover:text-secondary`}
+//               >
+//                 Home
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 href="#aboutUs"
+//                 className={`font-medium uppercase ${
+//                   isActive("/#aboutUs") ? "text-secondary" : "text-[#D2D2D0]"
+//                 } transition-all hover:text-secondary`}
+//               >
+//                 About
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 href="#services"
+//                 className={`font-medium uppercase ${
+//                   isActive("/#services") ? "text-secondary" : "text-[#D2D2D0]"
+//                 } transition-all hover:text-secondary`}
+//               >
+//                 Services
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 href="#contact"
+//                 className={`font-medium uppercase ${
+//                   isActive("/#contact") ? "text-secondary" : "text-[#D2D2D0]"
+//                 } transition-all hover:text-secondary`}
+//               >
+//                 Contact
+//               </a>
+//             </li>
+//             <li>
+//               <Link
+//                 to="/news"
+//                 className={`font-medium uppercase ${
+//                   isActive("/news") ? "text-secondary" : "text-[#D2D2D0]"
+//                 } transition-all hover:text-secondary`}
+//               >
+//                 News
+//               </Link>
+//             </li>
+//             <li>
+//               <Link
+//                 to="/testimonials"
+//                 className={`font-medium uppercase ${
+//                   isActive("/testimonials") ? "text-secondary" : "text-[#D2D2D0]"
+//                 } transition-all hover:text-secondary`}
+//               >
+//                 Testimonials
+//               </Link>
+//             </li>
+//             <li className="relative">
+//               <button
+//                 className={`flex gap-1 rounded-lg items-center font-medium uppercase ${
+//                   isDropdownOpen ? "text-secondary" : "text-[#D2D2D0]"
+//                 } transition-all hover:text-secondary`}
+//                 onClick={() => setIsDropdownOpen((prev) => !prev)}
+//               >
+//                 More
+//                 <FaChevronDown className="ml-1" />
+//               </button>
+//               {isDropdownOpen && (
+//                 <motion.div
+//                   ref={dropdownRef}
+//                   initial={{ opacity: 0, y: -10 }}
+//                   animate={{ opacity: 1, y: 0 }}
+//                   exit={{ opacity: 0, y: -10 }}
+//                   transition={{ duration: 0.3 }}
+//                   className="absolute left-0 mt-2 w-48 backdrop-blur-md bg-white/40 rounded-md shadow-lg z-10"
+//                 >
+//                   <ul className="flex flex-col">
+//                     <li>
+//                       <Link
+//                         to="/item1"
+//                         className="block px-4 py-2 text-white hover:bg-secondary font-medium uppercase rounded-md"
+//                         onClick={() => setIsDropdownOpen(false)}
+//                       >
+//                         Item 1
+//                       </Link>
+//                     </li>
+//                     <li>
+//                       <Link
+//                         to="/item2"
+//                         className="block px-4 py-2 text-white hover:bg-secondary font-medium uppercase rounded-md"
+//                         onClick={() => setIsDropdownOpen(false)}
+//                       >
+//                         Item 2
+//                       </Link>
+//                     </li>
+//                     <li>
+//                       <Link
+//                         to="/item3"
+//                         className="block px-4 py-2 text-white hover:bg-secondary font-medium uppercase rounded-md"
+//                         onClick={() => setIsDropdownOpen(false)}
+//                       >
+//                         Item 3
+//                       </Link>
+//                     </li>
+//                   </ul>
+//                 </motion.div>
+//               )}
+//             </li>
+//           </ul>
+//         </div>
+
+//         <div className="flex gap-7 h-full w-[50%] md:w-[10%] justify-end items-center">
+//           <FaRegEnvelope className="text-xl md:text-2xl text-[#D2D2D0]" />
+//           <HamburgerMenu />
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import { FaRegEnvelope, FaChevronDown } from "react-icons/fa";
@@ -561,10 +750,10 @@ import HamburgerMenu from "./hamburgar";
 const Navbar: React.FC = () => {
   const [scrolled, setScrolled] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState<string | null>(null); // New activeTab state
   const dropdownRef = useRef<HTMLDivElement | null>(null);
   const location = useLocation();
 
-  // Handle scroll effect for background change
   const handleScroll = () => {
     setScrolled(window.scrollY > 50);
   };
@@ -576,7 +765,6 @@ const Navbar: React.FC = () => {
     };
   }, []);
 
-  // Close dropdown if clicking outside
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
@@ -589,8 +777,6 @@ const Navbar: React.FC = () => {
     };
   }, []);
 
-  const isActive = (path: string) => location.pathname === path;
-
   // Conditionally render Navbar only on specific pages
   if (location.pathname === "/news" || location.pathname === "/testimonials") {
     return null;
@@ -599,82 +785,38 @@ const Navbar: React.FC = () => {
   return (
     <nav
       className={`fixed w-full top-0 left-0 z-[1000] transition-all duration-500 ease-out
-      ${
-        scrolled
-          ? "bg-white/5 bg-opacity-70 backdrop-blur-md shadow-lg h-[10%]"
-          : "bg-transparent h-[15%]"
-      } 
+      ${scrolled ? "bg-white/5 bg-opacity-70 backdrop-blur-md shadow-lg h-[10%]" : "bg-transparent h-[15%]"} 
       flex items-center justify-center px-8 lg:px-[10rem]`}
     >
       <div className="flex items-center w-full h-full">
         <div className="w-[50%] md:w-[20%] lg:w-[20%] h-full">
-          <Link to="/#home" className="w-full h-full flex justify-center items-center">
+          <a href="#home" className="w-full h-full flex justify-center items-center">
             <img src="./game_on_logo.webp" alt="Logo" className="w-full" />
-          </Link>
+          </a>
         </div>
 
         <div className="hidden bg-[#181817] w-[70%] lg:w-[70%] md:bg-transparent md:flex h-full items-center justify-center transition-all duration-500 ease-out">
           <ul className="flex flex-col items-center justify-center w-full gap-8 lg:gap-16 text-white md:flex-row md:w-auto font-secondary">
-            <li>
-              <Link
-                to="/#home"
-                className={`font-medium uppercase ${
-                  isActive("/#home") || location.pathname === "/" ? "text-secondary" : "text-[#D2D2D0]"
-                } transition-all hover:text-secondary`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/#aboutUs"
-                className={`font-medium uppercase ${
-                  isActive("/#aboutUs") ? "text-secondary" : "text-[#D2D2D0]"
-                } transition-all hover:text-secondary`}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/#services"
-                className={`font-medium uppercase ${
-                  isActive("/#services") ? "text-secondary" : "text-[#D2D2D0]"
-                } transition-all hover:text-secondary`}
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/#contact"
-                className={`font-medium uppercase ${
-                  isActive("/#contact") ? "text-secondary" : "text-[#D2D2D0]"
-                } transition-all hover:text-secondary`}
-              >
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/news"
-                className={`font-medium uppercase ${
-                  isActive("/news") ? "text-secondary" : "text-[#D2D2D0]"
-                } transition-all hover:text-secondary`}
-              >
-                News
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/testimonials"
-                className={`font-medium uppercase ${
-                  isActive("/testimonials") ? "text-secondary" : "text-[#D2D2D0]"
-                } transition-all hover:text-secondary`}
-              >
-                Testimonials
-              </Link>
-            </li>
+            {[
+              { path: "#home", label: "Home" },
+              { path: "#aboutUs", label: "About" },
+              { path: "#services", label: "Services" },
+              { path: "#contact", label: "Contact" },
+              { path: "/news", label: "News" },
+              { path: "/testimonials", label: "Testimonials" }
+            ].map((item) => (
+              <li key={item.label}>
+                <Link
+                  to={item.path}
+                  className={`font-medium uppercase ${
+                    activeTab === item.path ? "text-secondary" : "text-[#D2D2D0]"
+                  } transition-all hover:text-secondary`}
+                  onClick={() => setActiveTab(item.path)} // Update activeTab on click
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
             <li className="relative">
               <button
                 className={`flex gap-1 rounded-lg items-center font-medium uppercase ${
@@ -695,33 +837,24 @@ const Navbar: React.FC = () => {
                   className="absolute left-0 mt-2 w-48 backdrop-blur-md bg-white/40 rounded-md shadow-lg z-10"
                 >
                   <ul className="flex flex-col">
-                    <li>
-                      <Link
-                        to="/item1"
-                        className="block px-4 py-2 text-white hover:bg-secondary font-medium uppercase rounded-md"
-                        onClick={() => setIsDropdownOpen(false)}
-                      >
-                        Item 1
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/item2"
-                        className="block px-4 py-2 text-white hover:bg-secondary font-medium uppercase rounded-md"
-                        onClick={() => setIsDropdownOpen(false)}
-                      >
-                        Item 2
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to="/item3"
-                        className="block px-4 py-2 text-white hover:bg-secondary font-medium uppercase rounded-md"
-                        onClick={() => setIsDropdownOpen(false)}
-                      >
-                        Item 3
-                      </Link>
-                    </li>
+                    {[
+                      { path: "/item1", label: "Item 1" },
+                      { path: "/item2", label: "Item 2" },
+                      { path: "/item3", label: "Item 3" }
+                    ].map((item) => (
+                      <li key={item.label}>
+                        <Link
+                          to={item.path}
+                          className="block px-4 py-2 text-white hover:bg-secondary font-medium uppercase rounded-md"
+                          onClick={() => {
+                            setIsDropdownOpen(false);
+                            setActiveTab(item.path); // Update activeTab when clicking dropdown items
+                          }}
+                        >
+                          {item.label}
+                        </Link>
+                      </li>
+                    ))}
                   </ul>
                 </motion.div>
               )}
