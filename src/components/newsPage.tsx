9cabd019c954dad2984fd778f0bc973c5f39c973
gunavarmaOfficial <gunavarmaofficial00@gmail.com>
2024-10-30 19:40:55 +0530

    semi bold removed

--- src/components/newsPage.tsx
+++ src/components/newsPage.tsx
@@ -1,16 +1,20 @@
-import BrandsCarousel from "./brandCarousel/main";
 import { ExpandableCardDemo } from "./news/main";
+import TextHoverAnimation from "./textHoverAnimation";
 
 function News_Page() {
   return (
-    <div className="text-center flex-col flex gap-10 md:px-24 py-10">
-      <p className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-primary text-white font-semibold uppercase leading-tight">
-        News <span className="text-secondary">&</span> awards
+    <div className="text-center flex-col flex gap-10 md:pt-[10%] pt-32 px-32">
+      <p className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-primary flex gap-2 text-white  uppercase leading-tight">
+        <TextHoverAnimation text="News" />
+        <span className="text-secondary">
+          <TextHoverAnimation text="&" />
+        </span>
+        <TextHoverAnimation text="awards" />
       </p>
       <div className="border border-secondary"></div>
       {/* <NewsPage /> */}
       <ExpandableCardDemo />
-      <BrandsCarousel />
+      {/* <BrandsCarousel /> */}
     </div>
   );
 }
