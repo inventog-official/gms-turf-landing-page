73f04451ae89866fc5789be437c929562ea230b5
Nandha Official <nandha10799@gmail.com>
2024-11-01 02:14:13 +0530

    news page responsive bug fixing

--- src/components/newsPage.tsx
+++ src/components/newsPage.tsx
@@ -1,11 +1,11 @@
-import { Footer } from "./footer";
+import { FaFacebook, FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
 import { ExpandableCardDemo } from "./news/main";
 import TextHoverAnimation from "./textHoverAnimation";
 
 function News_Page() {
   return (
-    <div className="text-center flex-col flex gap-10 md:pt-[15%] pt-32 px-36">
-      <p className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-primary flex gap-2 text-white  uppercase leading-tight">
+    <div className="flex flex-col justify-start gap-5 select-none items-center min-h-screen w-full pt-36 md:pt-44 lg:pt-40 px-10 lg:px-48 pb-12">
+      <p className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-primary flex gap-2 text-white uppercase leading-tight">
         <TextHoverAnimation text="News" />
         <span className="text-secondary">
           <TextHoverAnimation text="&" />
@@ -16,7 +16,35 @@ function News_Page() {
       {/* <NewsPage /> */}
       <ExpandableCardDemo />
       {/* <BrandsCarousel /> */}
-      <Footer/>
+      <div className="w-full">
+        <div className="flex flex-col gap-10 mt-10">
+          <div className="w-full flex flex-col lg:flex-row border-t border-b border-slate-100/20">
+            <div className="py-10 lg:py-[3.5rem] px-3 lg:px-1 flex flex-col lg:flex-row w-[60%] text-[12px] gap-5 lg:gap-10 font-secondary font-semibold uppercase">
+              <p className="hover:cursor-pointer hover:text-white">Home</p>
+              <p className="hover:cursor-pointer hover:text-white">About</p>
+              <p className="hover:cursor-pointer hover:text-white">Services</p>
+              <p className="hover:cursor-pointer hover:text-white">Contact</p>
+            </div>
+            <div className="pb-10 lg:py-[3.5rem] px-3 lg:px-1 flex w-[40%] text-[12px] gap-8 font-secondary font-semibold lg:justify-end">
+              <div className="border p-2 border-slate-100/20 flex justify-center hover:bg-slate-500/20 hover:cursor-pointer hover:text-white hover:border-white">
+                <FaInstagram />
+              </div>
+              <div className="border p-2 border-slate-100/20 flex justify-center hover:bg-slate-500/20 hover:cursor-pointer hover:text-white hover:border-white">
+                <FaYoutube />
+              </div>
+              <div className="border p-2 border-slate-100/20 flex justify-center hover:bg-slate-500/20 hover:cursor-pointer hover:text-white hover:border-white">
+                <FaFacebook />
+              </div>
+              <div className="border p-2 border-slate-100/20 flex justify-center hover:bg-slate-500/20 hover:cursor-pointer hover:text-white hover:border-white">
+                <FaWhatsapp />
+              </div>
+            </div>
+          </div>
+          <div className="flex py-5">
+            <p>©2024. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
