0f9e27531442c1a598b2b4ceadbc349213408bf4
Nandha Official <nandha10799@gmail.com>
2024-11-08 21:02:52 +0530

    review client img bug fixing

--- src/common/reviewsObj/index.ts
+++ src/common/reviewsObj/index.ts
@@ -2,21 +2,21 @@ export const reviewsObj = [
   {
     review:
       "The turf is very spacious and large as compared to other turfs in the city.",
-    img: "public/TestimoniaImages/image1.webp",
+    img: "/TestimoniaImages/image1.webp",
   },
   {
     review:
       "Amazing way of explanation about the plan and way of approach got our trust onto them flawlessly.",
-    img: "public/TestimoniaImages/image4.webp",
+    img: "/TestimoniaImages/image4.webp",
   },
   {
     review:
       '"Building a turf in 1200 sq.fts. was just like a dream come true. Amazing work at best cost."',
-    img: "public/TestimoniaImages/image2.webp",
+    img: "/TestimoniaImages/image2.webp",
   },
   {
     review:
       '"Amazing way of explanation about the plan and way of approach got our trust onto them flawlessly."',
-    img: "public/TestimoniaImages/image3.webp",
+    img: "/TestimoniaImages/image3.webp",
   },
 ];
