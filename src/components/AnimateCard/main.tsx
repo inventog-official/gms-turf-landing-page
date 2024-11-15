9b8a85109fa67a1ddc536778df83b3b9bc4f5801
Tamil0105 <tamil16399@gmail.com>
2024-11-15 16:29:28 +0530

    animated card updated

--- src/components/AnimateCard/main.tsx
+++ src/components/AnimateCard/main.tsx
@@ -1,7 +1,8 @@
 // AnimatedCard.tsx
-import React, { useEffect, useRef } from 'react';
+import React, {  useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,38 +13,49 @@ interface AnimatedCardProps {
 const AnimatedCard: React.FC<AnimatedCardProps> = ({ children }) => {
     const cardRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const card = cardRef.current;
 
-        if (card) {
-            gsap.fromTo(card, 
-                { 
-                    opacity: 0, 
-                    y: -20, 
-                    scale: 1.2 
-                }, 
-                { 
-                    opacity: 1, 
-                    y: 0, 
-                    scale: 1, 
-                    duration: 1,  // Increased duration to 1.5 seconds
-                    ease: "power4.out",  // Changed easing to power4 for a smoother effect
-                    scrollTrigger: {
-                        trigger: card,
-                        start: "top 60%",
-                        toggleActions: "play none none reverse",
-                    }
-                }
-            );
-        }
-    }, []);
+    const {scrollYProgress,} =useScroll({
+        target:cardRef,
+        offset:['0 3',"0.9 1"]
+    })
+
+    const scaleProgress =  useTransform(scrollYProgress,[0,1],[0.5,1])
+    // useEffect(() => {
+    //     const card = cardRef.current;
+
+    //     if (card) {
+    //         gsap.fromTo(card, 
+    //             { 
+    //                 opacity: 0, 
+    //                 y: -20, 
+    //                 scale: 1.2 
+    //             }, 
+    //             { 
+    //                 opacity: 1, 
+    //                 y: 0, 
+    //                 scale: 1, 
+    //                 duration: 0.8,  // Increased duration to 1.5 seconds
+    //                 ease: "power4.out",  // Changed easing to power4 for a smoother effect
+    //                 scrollTrigger: {
+    //                     trigger: card,
+    //                     start: "top 60%",
+    //                     toggleActions: "play none none reverse",
+    //                 }
+    //             }
+    //         );
+    //     }
+    // }, []);
 
     return (
-        <div
-        //  ref={cardRef} 
-         className="card  flex justify-center duration-300 w-full transform translate-y-5">
+        <motion.div
+        style={{
+            scale:scaleProgress,
+            opacity:scrollYProgress
+        }}
+         ref={cardRef} 
+         className="card  flex justify-center duration-800 w-full transform translate-y-5">
             {children}
-        </div>
+        </motion.div>
     );
 };
 
