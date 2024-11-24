b1cc8d1814c8ca935578282c741f3e2d99c7afa6
Nandha Official <nandha10799@gmail.com>
2024-11-24 14:16:58 +0530

    changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -45,7 +45,7 @@ export const Footer = () => {
   const handleNavClick = (path: string) => {
     if (path.startsWith("#")) {
       if (location.pathname !== "/") {
-        console.log(9);
+        console.log(33333);
         // If not on the home page, navigate to it first
         navigate("/", { replace: true });
         setTimeout(() => {
