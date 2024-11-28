39fd5948c477ea5bd67c76bcc474b741e36cfdcb
Tamil0105 <tamil16399@gmail.com>
2024-11-28 22:14:55 +0530

    whatsup phone number updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -114,7 +114,7 @@ export const Footer = () => {
             </div> */}
             <div
               onClick={() => {
-                window.open("https://wa.me/9615737373");
+                window.open("https://wa.me/9715131313");
               }}
               className="border p-2 border-slate-100/20 flex justify-center hover:bg-slate-500/20 hover:cursor-pointer hover:text-white hover:border-white"
             >
